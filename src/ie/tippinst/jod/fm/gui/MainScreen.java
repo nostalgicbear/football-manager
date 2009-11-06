63d507a654c348078434d32e735e3c819aba0921
josephod89 <josephod89@823fb9be-bce2-11de-8784-f7ad4773dbab>
2009-11-06 16:40:06 +0000

    Renamed GUI package

---
+++ src/ie/tippinst/jod/fm/gui/MainScreen.java
@@ -0,0 +1,53 @@
+package ie.tippinst.jod.fm.gui;
+
+import ie.tippinst.jod.fm.app.Game;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainScreen extends JFrame {
+	
+	private static final long serialVersionUID = -3630689263806675808L;
+	private JButton newGame;
+	private JButton newUser;
+	private Game game;
+	
+	public MainScreen(){
+		super("Football Manager");
+		game = new Game();
+		newGame = new JButton("Start New Game");
+		newUser = new JButton("Create New User");
+		this.setLayout(new FlowLayout());
+		this.getContentPane().add(newGame);
+		this.getContentPane().add(newUser);
+		this.setSize(300,300);
+		this.setVisible(true);
+		
+		newGame.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.loadDatabase();
+				System.out.println("Database loaded");
+			}
+			
+		});
+		
+		/*newUser.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game.createNewUser();
+				System.out.println("New User created");
+			}
+			
+		});*/
+	}
+	
+	
+
+}
